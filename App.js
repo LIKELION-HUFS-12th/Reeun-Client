c9fa6fd6ba7a24ff591378d4e7a1f962e7847a73
wh0land <cliee0730@gamil.com>
2024-12-28 15:38:12 +0900

    App.js

--- App.js
+++ App.js
@@ -1,28 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< Updated upstream
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-=======
 import HomeScreen from './src/screens/Home';
 import { ThemeProvider } from 'styled-components/native'; // Theme Provider 사용해서 색상 전역으로 사용!!
 import { color } from './src/styles/Color';
-import Chat from './src/screens/Chat';
 
 export default function App() {
   return (
     <ThemeProvider theme={color}>
       <View style={styles.container}>
-        <Chat/>
+        <HomeScreen />
         <StatusBar style="auto" />
       </View>
     </ThemeProvider>
->>>>>>> Stashed changes
   );
 }
 
